e35f6c983d3aa0dbfb6a253e75354d1178098543
huzhiqian <814397842@qq.com>
2019-05-12 21:45:54 +0800

    Add Managed Extensibility Framework Demo

---
+++ ManagedExtensibilityFramework/简单MEF测试/SimpleMEFDemo/SimpleMEFDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace SimpleMEFDemo
+{
+    class Program
+    {
+        [Import]
+        public IWorkService work { get; set; }
+
+        static void Main(string[] args)
+        {
+            Program pro = new Program();
+            pro.Compose();
+            if (pro.work != null)
+            {
+                pro.work.WorkName = "工程师";
+                Console.WriteLine(pro.work.GetWorkName());
+
+                //
+                pro.work.DoWork();
+            }
+            Console.ReadLine();
+        }
+
+        private void Compose()
+        {
+            // MEF提供三种方式发现部件
+            // 1:AssemblyCatalog 在当前程序集发现部件。
+            // 2:DirectoryCatalog 在指定的目录发现部件。
+            // 3:DeploymentCatalog 在指定的XAP文件中发现部件（用于silverlight）
+            //
+            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            CompositionContainer container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+        }
+    }
+}
