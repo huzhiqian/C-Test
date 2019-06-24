48cf91b1a5e2a3ff9f3e1d5f16b71b99b9d159af
huzhiqian <814397842@qq.com>
2019-06-24 20:16:13 +0800

    add new log4net test proj

--- Log4Net测试程序/Log4Net_TestProj1/Log4Net_TestProj1/Program.cs
+++ Log4Net测试程序/Log4Net_TestProj1/Log4Net_TestProj1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using log4net;
 
 namespace Log4Net_TestProj1
 {
@@ -10,8 +12,17 @@ namespace Log4Net_TestProj1
     {
         static void Main(string[] args)
         {
+            string logConfig = ConfigurationManager.AppSettings["log4net"];
+            Console.WriteLine(logConfig);
+            var fi = new System.IO.FileInfo(logConfig);
 
+            log4net.Config.XmlConfigurator.Configure(fi);
 
+             log4net.ILog Loginfo = log4net.LogManager.GetLogger("loginfo");
+          
+            Loginfo.Info("Error:123",new Exception("error"));
+
+        Console.ReadLine();
         }
     }
 }
