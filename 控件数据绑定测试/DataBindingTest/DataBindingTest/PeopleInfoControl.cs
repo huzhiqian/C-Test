6b208d2fdb5ea7b5c9db703a218c9d1199f57abe
huzhiqian <814397842@qq.com>
2020-07-18 12:06:56 +0800

    add new Test

---
+++ 控件数据绑定测试/DataBindingTest/DataBindingTest/PeopleInfoControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataBindingTest
+{
+    public partial class PeopleInfoControl : UserControl
+    {
+        public PeopleInfoControl()
+        {
+            InitializeComponent();
+        }
+
+
+        public IPeopleInfo DataBindingObj
+        {
+            set {
+                this.tbx_Name.DataBindings.Add("Text",value,"Name");
+                this.tbx_Age.DataBindings.Add("Text",value,"Age");
+                this.cmb_Sex.DataBindings.Add("Text",value,"Sex");
+                this.tbx_Address.DataBindings.Add("Text",value,"Address");
+            }
+        }
+
+       
+    }
+}
